f65b7e5f56c4e302b4b854430cdada889fe1188e
ok-tomo <ok.tmk13131@gmail.com>
2022-03-13 16:44:56 +0900

    fix: 配列の初期化と数え上げを修正

--- src/Component/Board.tsx
+++ src/Component/Board.tsx
@@ -4,15 +4,9 @@ import Square from './Square';
 import mapGen from './MapGen';
 
 const Board: React.VFC = () => {
-    const boardSize: number = 20;
-    const boardData: number[][] = Array(boardSize).fill([
-        -1, 1, 2, 3, 4, 5, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    ]);
-    const map = mapGen(20, 1);
-    console.log(map);
+    const map = mapGen(20, 50);
 
-    const board = boardData.map((item) => {
+    const board = map.map((item) => {
         const row = item.map((s: number) => <Square status={s} />);
 
         return <div className="row">{row}</div>;
--- src/Component/MapGen.ts
+++ src/Component/MapGen.ts
@@ -1,3 +1,4 @@
+// 周辺の爆弾の数を数える
 const bombCount = (map: number[][]) => {
     const newMap: number[][] = map;
     const size = map.length;
@@ -5,12 +6,13 @@ const bombCount = (map: number[][]) => {
     // O(size ^ 2)
     for (let y = 0; y < size; y += 1) {
         for (let x = 0; x < size; x += 1) {
+            if (newMap[y][x] >= 0) { continue; }
+
             for (let dy = -1; dy < 2; dy += 1) {
                 if ((y === 0 && dy === -1) || (y === size - 1 && dy === 1)) { continue; }
 
                 for (let dx = -1; dx < 2; dx += 1) {
                     if ((x === 0 && dx === -1) || (x === size - 1 && dx === 1)) { continue; }
-
                     if (dy === 0 && dx === 0) { continue; }
                     const nx: number = x + dx;
                     const ny: number = y + dy;
@@ -25,12 +27,12 @@ const bombCount = (map: number[][]) => {
     return newMap;
 };
 
-const mapGen = (size: number, bombNum: number) => {
-    let map = Array(size).fill(Array(size).fill(0));
-    const bombPoint = [...Array(size)].map(() => new Array(size).fill(0));
-    let bombs: number = bombNum;
+const bombGen = (size: number, bombs: number) => {
+    const map = [...Array(size)].map(() => new Array(size).fill(0));
+    const bombPoint: number[][] = Array(0);
+    let n: number = bombs;
 
-    for (let i = 0; i < bombs; i += 1) {
+    for (let i = 0; i < n; i += 1) {
         // sizeの範囲内で爆弾の生成
         const x: number = Math.floor(Math.random() * size);
         const y: number = Math.floor(Math.random() * size);
@@ -44,13 +46,21 @@ const mapGen = (size: number, bombNum: number) => {
 
         // 被った場合はループを1増やして次へ
         if (isAlreadyAdded) {
-            bombs += 1;
+            n += 1;
             continue;
         }
 
         map[y][x] = -1;
         bombPoint.push([x, y]);
     }
+
+    return map;
+};
+
+// マップの生成
+const mapGen = (size: number, bombs: number) => {
+    // マップの初期化と爆弾の配置
+    let map = bombGen(size, bombs);
 
     // 周囲の爆弾の数を数える
     map = bombCount(map);
